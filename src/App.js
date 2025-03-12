17811a8a83964e2718a707ccaf1968b85dd71586
Karthik Avula <96226768+avula-karthik@users.noreply.github.com>
2025-03-12 22:23:40 +0530

    last_commit
    last_commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h3>Feature A added !!</h3>
-      <h1>AWS GANG !!! </h1>
-      <h6>New features added by AWS Gang </h6>
+      <h1>Last commit for the day! </h1>
     </div>
   );
 }
